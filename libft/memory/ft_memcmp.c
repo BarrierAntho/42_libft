9d3855babfa4852a83a8ffa9cec6ba571ef32014
Anthony BARRIER <abarrier@e2r9p10.clusters.42paris.fr>
2022-05-16 11:27:46 +0200

    INIT - memcmp
    BODY:
    Init new function memcmp
    
    TODO:
    N/A

--- libft/memory/ft_memcmp.c
+++ libft/memory/ft_memcmp.c
@@ -1,21 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memory.h                                        :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/11 08:12:00 by abarrier          #+#    #+#             */
-/*   Updated: 2022/05/16 10:30:06 by abarrier         ###   ########.fr       */
+/*   Created: 2021/11/25 10:00:18 by abarrier          #+#    #+#             */
+/*   Updated: 2022/05/16 10:22:47 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_MEMORY_H
-# define FT_MEMORY_H
+#include "ft_memory.h"
 
-# include <stdlib.h>
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*ucs1;
+	unsigned char	*ucs2;
+	int				i;
 
-char	*ft_free_ptrptr_str(char **s);
-int	ft_memcmp(const void *s1, const void *s2, size_t n);
-
-#endif
+	ucs1 = (unsigned char *)s1;
+	ucs2 = (unsigned char *)s2;
+	i = 0;
+	while (i < (int) n)
+	{
+		if (ucs1[i] != ucs2[i])
+			return ((unsigned char)ucs1[i] - ucs2[i]);
+		i++;
+	}
+	return (0);
+}
