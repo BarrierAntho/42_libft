bf2955a94d3dcdf414c46dec396624bd2488edc8
Anthony BARRIER <abarrier@e2r9p10.clusters.42paris.fr>
2022-05-17 08:24:32 +0200

    UPDT - add del function argument
    BODY:
    Add del function argument to delete correctly the content of the object
    
    TODO:
    N/A

--- libft/list/ft_lst_clear.c
+++ libft/list/ft_lst_clear.c
@@ -6,13 +6,13 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 09:19:56 by abarrier          #+#    #+#             */
-/*   Updated: 2022/05/16 09:38:21 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/05/17 08:23:27 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 
-void	ft_lst_clear(t_list **lst)
+void	ft_lst_clear(t_list **lst, void (*f)(void *))
 {
 	t_list	*obj;
 	t_list	*del;
@@ -25,6 +25,8 @@ void	ft_lst_clear(t_list **lst)
 	{
 		del = obj;
 		obj = obj->next;
+		if (f)
+			(f)(del->content);
 		free(del);
 		del = NULL;
 	}
